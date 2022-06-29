e5abce43ac79ca434d592d330e4dcea9eba421a2
SmolBoy <65689332+IAmSmolBoy@users.noreply.github.com>
2022-06-29 12:26:19 +0800

    Made changes to icon location

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { userList } from './models/mock-user';
-import { User } from './models/user';
+import { userList } from '../models/mock-user';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
