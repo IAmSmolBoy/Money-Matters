eb179f7b7b3773d81c7f02964a8c63f1d1c8a339
SmolBoy <65689332+IAmSmolBoy@users.noreply.github.com>
2022-07-13 14:09:50 +0800

    -

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Transaction } from 'src/app/models/transaction';
 import { TransactionService } from 'src/app/services/transaction.service';
 
@@ -12,9 +11,10 @@ export class ReportComponent implements OnInit {
 
   constructor(private ts: TransactionService) { }
 
-  userId: number = parseInt(sessionStorage.getItem("userId") || "-1")
+  userId: number = parseInt(sessionStorage.getItem("userId")!)
 
   ngOnInit(): void {
+    console.log(this.transactionList[0].date.toDateString())
   }
 
   transactionList: Transaction[] = this.ts.getUserTransactions(this.userId)
